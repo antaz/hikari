e716c127428e37f3c8b6f974209dba15fcb7331c
raichoo <raichoo@acmelabs.space>
2020-06-20 11:13:47 +0000

    only schedule new frames when views are dirty
    This prevents clean views to schedule new frames when they are not visible
    anymore and don't need to advance their state through pending operations.

--- src/xdg_view.c
+++ src/xdg_view.c
@@ -98,7 +98,7 @@ commit_handler(struct wl_listener *listener, void *data)
       if (visible) {
         hikari_view_damage_whole(view);
       } else if (output->enabled) {
-        wlr_output_schedule_frame(output->wlr_output);
+        hikari_view_schedule_frame(view);
       }
     } else if (output->enabled) {
       if (visible) {
@@ -109,7 +109,7 @@ commit_handler(struct wl_listener *listener, void *data)
         wlr_output_damage_add(output->damage, &damage);
         pixman_region32_fini(&damage);
       } else {
-        wlr_output_schedule_frame(output->wlr_output);
+        hikari_view_schedule_frame(view);
       }
     }
   }
