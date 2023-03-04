b0ebd7e5f0def69dbd44d84590ef47f7f8137d96
blocr <blocr@proton.me>
2023-03-04 15:13:44 +0100

    prevent crashing with GTK4 apps
    author: Ashish SHUKLA <ashish@FreeBSD.org> 2022-07-10 13:53:29 +0000
    https://cgit.freebsd.org/ports/commit/?id=5d9c5b9cac42a22716f292fca5717c9842fdf464

--- src/server.c
+++ src/server.c
@@ -523,6 +523,9 @@ static void
 server_decoration_handler(struct wl_listener *listener, void *data)
 {
   struct wlr_server_decoration *wlr_decoration = data;
+  if (wlr_decoration->surface->role == NULL) {
+      return;
+  }
   struct hikari_view *view =
       wl_container_of(wlr_decoration->surface, view, surface);
   struct wlr_xdg_surface *xdg_surface =
